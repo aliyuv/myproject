caf065c13504daf097ca8f8f69783fc41a03adef
aliyu <1324149139@qq.com>
2023-01-07 19:36:02 +0800

    简单的练习了axios

---
+++ JavaScript高级/vite-demo-1/my-vue-app/main.js
@@ -0,0 +1,14 @@
+import axios from 'axios'
+
+const btn = document.querySelector('.btn')
+// axios.defaults.headers.common['Authorization'] = 'AUTH_TOKEN';
+
+btn.addEventListener('click', () => {
+  console.log(111)
+  // axios.get('/user')
+  //   .then(res => console.log(res))
+  //   .catch(err => console.log(err))
+  // axios.defaults.baseURL = 'https://mock.apifox.cn/m1/2170211-0-default'
+  // axios.defaults.headers.post['Content-Type'] = 'application/json';
+  // axios.post('/test',{name: 'test'})
+})
