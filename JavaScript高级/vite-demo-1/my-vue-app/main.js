77adc276603d9cb2a860c8be6769e4cb4287dda3
1324149139@qq.com <aliyu>
2023-01-09 12:52:23 +0800

    学习位运算

--- JavaScript高级/vite-demo-1/my-vue-app/main.js
+++ JavaScript高级/vite-demo-1/my-vue-app/main.js
@@ -1,14 +1,24 @@
-import axios from 'axios'
+// import axios from 'axios'
 
-const btn = document.querySelector('.btn')
-// axios.defaults.headers.common['Authorization'] = 'AUTH_TOKEN';
+// const btn = document.querySelector('.btn')
+// // axios.defaults.headers.common['Authorization'] = 'AUTH_TOKEN';
 
-btn.addEventListener('click', () => {
-  console.log(111)
-  // axios.get('/user')
-  //   .then(res => console.log(res))
-  //   .catch(err => console.log(err))
-  // axios.defaults.baseURL = 'https://mock.apifox.cn/m1/2170211-0-default'
-  // axios.defaults.headers.post['Content-Type'] = 'application/json';
-  // axios.post('/test',{name: 'test'})
-})
+// btn.addEventListener('click', () => {
+//   console.log(111)
+//   // axios.get('/user')
+//   //   .then(res => console.log(res))
+//   //   .catch(err => console.log(err))
+//   // axios.defaults.baseURL = 'https://mock.apifox.cn/m1/2170211-0-default'
+//   // axios.defaults.headers.post['Content-Type'] = 'application/json';
+//   // axios.post('/test',{name: 'test'})
+// })
+
+
+//位运算学习
+
+// const isEven = (num) => {
+//     return (num & 1) === 0 ? '偶数' : '奇数'
+// }
+
+// console.log(` 125 is ${isEven(125)}`)
+// console.log(` 8 is ${isEven(8)} `)
